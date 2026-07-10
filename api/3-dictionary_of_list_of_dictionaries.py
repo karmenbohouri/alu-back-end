6effabddd23585266fb9041e67b97c094f023958
karmenbohouri <karmenbohouri@alustudent.com>
2026-07-10 22:22:04 +0000

    alu-back-end

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,43 +1,37 @@
 #!/usr/bin/python3
-"""
-This module retrieves TODO list progress for all employees from a REST API
-and exports it to a JSON file.
+"""Export to JSON - all employees.
+
+This module fetches the TODO list of all employees from the
+JSONPlaceholder REST API and exports every task to a single JSON
+file named todo_all_employees.json.
 """
 import json
 import requests
 
+
 if __name__ == "__main__":
-    # Fetch all users
-    users_url = "https://jsonplaceholder.typicode.com/users"
-    users_response = requests.get(users_url)
-    users = users_response.json()
-    
-    # Prepare data for JSON
+    base_url = "https://jsonplaceholder.typicode.com"
+    users_url = "{}/users".format(base_url)
+    users = requests.get(users_url).json()
+
     all_tasks = {}
-    
     for user in users:
-        user_id = str(user.get('id'))
-        username = user.get('username')
-        
-        # Fetch TODO list for each user
-        todos_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
-        todos_response = requests.get(todos_url)
-        todos = todos_response.json()
-        
-        # Create task list for the user
-        tasks = [
-            {
+        user_id = user.get("id")
+        username = user.get("username")
+
+        todos_url = "{}/users/{}/todos".format(base_url, user_id)
+        todos = requests.get(todos_url).json()
+
+        tasks = []
+        for task in todos:
+            tasks.append({
                 "username": username,
-                "task": task.get('title'),
-                "completed": task.get('completed')
-            }
-            for task in todos
-        ]
-        
-        # Add to the dictionary
-        all_tasks[user_id] = tasks
-    
-    # Write to JSON file
+                "task": task.get("title"),
+                "completed": task.get("completed")
+            })
+
+        all_tasks[str(user_id)] = tasks
+
     filename = "todo_all_employees.json"
-    with open(filename, 'w') as f:
-        json.dump(all_tasks, f)
+    with open(filename, "w") as json_file:
+        json.dump(all_tasks, json_file)
